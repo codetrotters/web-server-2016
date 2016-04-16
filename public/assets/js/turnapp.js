df2c0e6c39fd37b44b8a90971bc8e0fd7ec3dacd
GabyRive <gabrielarivera@Gabrielas-MacBook.local>
2016-04-16 13:13:30 -0400

    Add row button
    Added add row button:
    -turn-app.html
    -turnapp.js

--- public/assets/js/turnapp.js
+++ public/assets/js/turnapp.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
-    var t = $('#example').DataTable();
-    var counter = 1;
 
-    $('#addRow').on( 'click', function () {
-        t.row.add( [
-            counter +'.1',
-            counter +'.2',
-            counter +'.3',
-            counter +'.4',
-            counter +'.5'
-        ] ).draw( false );
+  var date = getFullDate();
 
-        counter++;
-    } );
+  $('#date h4').text(date);
+  console.log(date);
 
-    // Automatically add a first row of data
-    $('#addRow').click();
+
 } );
+
+getFullDate = function() {
+  var today = new Date();
+  var days = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return days[today.getDay()] + ', ' + months[today.getMonth()] + " " + today.getDate() + ", " + today.getUTCFullYear();
+}
+
+function myFunction() {
+    var table = document.getElementById("myTable");
+    var row = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "NEW CELL1";
+    cell2.innerHTML = "NEW CELL2";
+}
